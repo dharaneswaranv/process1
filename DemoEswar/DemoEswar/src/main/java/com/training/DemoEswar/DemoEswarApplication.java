a5262e2e4587a042c87f83b9dbe8f1de360cfdd3
dharaneswaranv <125242423+dharaneswaranv@users.noreply.github.com>
2024-08-21 21:02:03 +0530

    Add files via upload

---
+++ DemoEswar/DemoEswar/src/main/java/com/training/DemoEswar/DemoEswarApplication.java
@@ -0,0 +1,13 @@
+package com.training.DemoEswar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEswarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoEswarApplication.class, args);
+	}
+
+}
---
+++ DemoEswar/DemoEswar/src/test/java/com/training/DemoEswar/DemoEswarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.DemoEswar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoEswarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
